b6fb760ca273903d2917fd0edd4daa614f476c5c
Nafees143 <menafees143@gmail.com>
2021-12-28 00:10:12 +0600

    o mago heiya

--- src/test/java/testbd/com/TC_001.java
+++ src/test/java/testbd/com/TC_001.java
@@ -1,6 +1,9 @@
 package testbd.com;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import bd.com.DriverSetup;
@@ -14,9 +17,16 @@ public class TC_001 extends DriverSetup {
 		driver.get(baseUrl);
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
+		
+		//Title verification
+		String expectedTitle = "Online Shopping in Bangladesh: Order Now from Daraz.com.bd";
+		String actualTitle = driver.getTitle();
+		
+		Assert.assertEquals(expectedTitle, actualTitle);
 
+		//Login part
 		driver.findElement(By.xpath("//a[contains(text(),'Signup / Login')]")).click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 
 		driver.findElement(By.xpath("//input[@placeholder='Please enter your Phone Number or Email']"))
 				.sendKeys("01722325384");
@@ -26,8 +36,25 @@ public class TC_001 extends DriverSetup {
 		Thread.sleep(2000);
 
 		driver.findElement(By.xpath("//button[contains(text(),'LOGIN')]")).click();
+		Thread.sleep(2000);
+		
+		//Menu bar part
+		driver.findElement(By.xpath("//span[contains(text(),'Electronic Devices')]")).click();
+		Thread.sleep(2000);
+		//Hover
+		WebElement hover = driver.findElement(By.xpath("//li[@data-cate='cate_1_4']/a/span[contains(text(),'Laptops')]"));
+		Actions action = new Actions(driver);
+		action.clickAndHold(hover).build().perform();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//span[contains(text(),'Asus Laptops')]")).click();
 		Thread.sleep(3000);
-
+		
+		//go to home
+		driver.findElement(By.xpath("//a[@data-spm='dhome']")).click();
+		Thread.sleep(2000);
+		
+		//Logout part
 		driver.findElement(By.xpath("//span[@id='myAccountTrigger']")).click();
 		Thread.sleep(2000);
 
@@ -35,5 +62,5 @@ public class TC_001 extends DriverSetup {
 		Thread.sleep(5000);
 
 	}
-
+	
 }
